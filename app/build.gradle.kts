e24ea544878e33c7d2e6a8f0f43fe3b5391c41af
dzakicok <dzakirofiandri39@gmail.com>
2025-10-21 14:49:26 +0700

    paling baru

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,10 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.cardview:cardview:1.0.0")
+// Versi bisa lebih baru
+    implementation("com.google.android.material:material:1.12.0")
+// Versi bisa lebih baru
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
